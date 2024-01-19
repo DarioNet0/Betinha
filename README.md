1e607bc9fa41ef718d3b7aab8659a32523f6506e
DarioNet0 <dariooliveirasneto@gmail.com>
2024-01-18 22:39:42 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Betinha
+# Betinha
+# Betinha
---
+++ main.js
@@ -0,0 +1,12 @@
+function betinha() {
+    window.location.href = "./Robertinha/robertinha.html"
+  } 
+  function thomas() {
+    window.location.href = "./Thomas/thomas.html"
+  } 
+  function fototetas() {
+    window.location.href = "./Fototetas/fototetas.html"
+  }
+  function convite() {
+    window.location.href = "./Convite/convite.html"
+  } 
